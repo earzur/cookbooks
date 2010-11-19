42c469407249440ee6a4a4c084c6d0f906588277
Erwan Arzur <earzur@runmyprocess.com>
2010-11-19 14:03:22 +0100

    the daemon off thing was really getting in my way. Found no way to not put this line in the config, even when node[:nginx][:daemon_disable] == false !

--- nginx/attributes/default.rb
+++ nginx/attributes/default.rb
@@ -1,7 +1,7 @@
 default[:nginx][:version]      = "0.7.67"
 default[:nginx][:install_path] = "/opt/nginx-#{nginx[:version]}"
 default[:nginx][:src_binary]   = "#{nginx[:install_path]}/sbin/nginx"
-
+default[:nginx][:daemon_disable] = false
 case platform
 when "debian","ubuntu"
   set[:nginx][:dir]     = "/etc/nginx"
